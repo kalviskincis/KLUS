2d2600222b1c6d359af566c4a35868380055ce0b
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2020-08-25 15:45:05 +0300

    lala

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ const vielasPoga = document.getElementById('vielas-poga');
 vielasPoga.addEventListener('click',function(){
     alert("Piespiesta vielas poga");
 })
+
+function klikskis() {
+    alert("Piespiesta vielas poga");
+}
